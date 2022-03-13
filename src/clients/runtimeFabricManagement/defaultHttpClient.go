071808a12553525ed066108b3fa3ac6f050d16f4
Alvaro Rubio <aljrubior@gmail.com>
2022-03-13 18:30:12 -0300

    implemented http client - rtf management

---
+++ src/clients/runtimeFabricManagement/defaultHttpClient.go
@@ -0,0 +1,32 @@
+package runtimeFabricManagement
+
+import (
+	"github.com/aljrubior/amc-ui-rest-facade/clients"
+	"github.com/aljrubior/amc-ui-rest-facade/config"
+	"sync"
+)
+
+var lock = &sync.Mutex{}
+
+var (
+	instance *DefaultHttpClient
+)
+
+func NewDefaultHttpClient(config config.RuntimeFabricManagementClientConfig) DefaultHttpClient {
+
+	lock.Lock()
+	defer lock.Unlock()
+
+	if instance == nil {
+		instance = &DefaultHttpClient{
+			config: config,
+		}
+	}
+
+	return *instance
+}
+
+type DefaultHttpClient struct {
+	clients.BaseHttpClient
+	config config.RuntimeFabricManagementClientConfig
+}
