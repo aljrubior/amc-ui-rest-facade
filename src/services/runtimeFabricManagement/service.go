9eb901b146740d5a6dc922fadc2b76fdb8cd12a7
Alvaro Rubio <aljrubior@gmail.com>
2022-03-13 18:39:19 -0300

    implemented get targets - rtf mangement service

--- src/services/runtimeFabricManagement/service.go
+++ src/services/runtimeFabricManagement/service.go
@@ -2,6 +2,6 @@ package runtimeFabricManagement
 
 import "github.com/aljrubior/amc-ui-rest-facade/clients/runtimeFabricManagement/responses"
 
-type HttpClient interface {
+type Service interface {
 	GetTargets(token, orgId, envId string) (*[]responses.TargetResponse, error)
 }
